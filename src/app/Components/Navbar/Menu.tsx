f825483ea2cdfd9a1a7e4b4ac3e8a7da9fc00df4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-24 01:16:31 -0500

    fix: menu styles (after typography be actual used)

--- src/app/Components/Navbar/Menu.tsx
+++ src/app/Components/Navbar/Menu.tsx
@@ -24,23 +24,35 @@ const Menu = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primary rounded-box absolute top-10 right-0 w-28 flex flex-col items-center"
               onMouseLeave={() => setShowMenu(false)}
             >
-              <li className="text-secondary hover:underline underline-offset-4">
-                <Link href="#hero">
+              <li>
+                <Link
+                  href="#hero"
+                  className="no-underline text-secondary hover:underline underline-offset-4"
+                >
                   <button>Home</button>
                 </Link>
               </li>
-              <li className="text-secondary hover:underline underline-offset-4">
-                <Link href="#skills">
+              <li>
+                <Link
+                  href="#skills"
+                  className="no-underline text-secondary hover:underline underline-offset-4"
+                >
                   <button>Skills</button>
                 </Link>
               </li>
-              <li className="text-secondary hover:underline underline-offset-4">
-                <Link href="#portfolio">
+              <li>
+                <Link
+                  href="#portfolio"
+                  className="no-underline text-secondary hover:underline underline-offset-4"
+                >
                   <button>Portfolio</button>
                 </Link>
               </li>
-              <li className="text-secondary hover:underline underline-offset-4">
-                <Link href="#history">
+              <li>
+                <Link
+                  href="#history"
+                  className="no-underline text-secondary hover:underline underline-offset-4"
+                >
                   <button>History</button>
                 </Link>
               </li>
