374df282892c12c6739988d5a6aafff44bf9c47b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-23 22:35:45 -0500

    feat: update project urls

--- src/app/Components/Portfolio/index.tsx
+++ src/app/Components/Portfolio/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PortfolioItem from "./PortfolioItem";
 
 export interface PortfolioList {
@@ -12,15 +13,15 @@ const portfolioList: PortfolioList[] = [
     name: "No Border Jobs",
     description:
       "Job platform focused exclusively in remote and global opportunities.",
-    videoUrl: "https://www.youtube.com/embed/zhKt7Hcd5ME?si=DLHFES8CpTE7zwr7", //Replace for the right URL
+    videoUrl: "https://www.youtube.com/embed/t2rsif_PT7w?si=MlMYb7-BDhWR4utr", //Replace for the right URL
     projectUrl: "https://www.nobordersjobs.com/",
   },
   {
     name: "SEO Helper",
     description:
       "Wep app to help with SEO, providing hashtags and image to any kind of social media",
-    videoUrl: "https://www.youtube.com/embed/zhKt7Hcd5ME?si=DLHFES8CpTE7zwr7", //Replace for the right URL
-    projectUrl: "https://noborderjobs.com/",
+    videoUrl: "https://www.youtube.com/embed/zqw8P6qM2aw?si=vFUIFfj6Twf6eaJQ",
+    projectUrl: "https://hashtag-thumbnail-generator.vercel.app/",
   },
 ];
 
@@ -30,9 +31,23 @@ const Portfolio = () => (
     className="flex flex-col justify-center items-center gap-5 pt-20"
   >
     <h2 className="text-2xl font-bold">Featured Projects</h2>
-    {portfolioList.map((portfolioItem , index, arr) => (
-      <PortfolioItem portfolioItem={portfolioItem} isLast={index === arr.length - 1}/>
+    {portfolioList.map((portfolioItem, index, arr) => (
+      <PortfolioItem
+        portfolioItem={portfolioItem}
+        isLast={index === arr.length - 1}
+      />
     ))}
+    <p>
+      My old projects (from 2021 and bellow) is available on my{" "}
+      <Link
+        href="https://pedroalmeidaprogrammer-2021.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-info underline"
+      >
+        previous website
+      </Link>
+    </p>
   </section>
 );
 
