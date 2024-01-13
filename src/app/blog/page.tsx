fcf4d4af8670bd97774b603d567329f2f5ad4e2b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-13 14:32:33 -0500

    initial copy on blog main page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 const BlogPage = () => {
   return (
-    <main>
-      <h1>Blogs</h1>
+    <main className="px-3 mt-3">
+      <h1>Blog Posts</h1>
+      <h2>Check out my articles!</h2>
       <ul>
         <li>
           <Link href="/blog/handling-with-promises">
