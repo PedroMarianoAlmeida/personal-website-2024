42f3132b92d61ee18cbfb96fb1d6265e33dbb4b7
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-14 22:58:50 -0500

    fix eslint

--- src/app/blog/async-wrapper/Sessions/PreRequisites.tsx
+++ src/app/blog/async-wrapper/Sessions/PreRequisites.tsx
@@ -2,7 +2,7 @@ const PreRequisites = () => (
   <>
     <h2>Pre requisites</h2>
     <p>
-      This code requires TypeScript. While it's possible to use in a Vanilla
+      This code requires TypeScript. While it{"'"}s possible to use in a Vanilla
       JavaScript project, it might not add significant benefits due to the extra
       complexity.
     </p>
