2482293d4ae9242177a5bd5b325e86121cb5d09a
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-14 23:47:53 -0500

    small adjust on async-wrapper post

--- src/app/blog/async-wrapper/Sessions/PreRequisites.tsx
+++ src/app/blog/async-wrapper/Sessions/PreRequisites.tsx
@@ -3,8 +3,8 @@ const PreRequisites = () => (
     <h2>Pre requisites</h2>
     <p>
       This code requires TypeScript. While it{"'"}s possible to use in a Vanilla
-      JavaScript project, it might not add significant benefits due to the extra
-      complexity.
+      JavaScript project (if you adapt the code), it might not add significant
+      benefits due to the extra complexity.
     </p>
   </>
 );
